b2a5187c11bb3aee737a08cc3d64b0c50f4a843f
Albert Czarnecki <abath88@gmail.com>
2023-07-29 10:09:13 +0200

    Update dwa.js

--- dwa.js
+++ dwa.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const date = '2022-12-23';
 
-/*async function myaxios(url) {
+async function myaxios(url) {
     return new Promise((resolve, reject) =>
         axios({
             method: 'get',
@@ -12,11 +12,28 @@ const date = '2022-12-23';
         ).catch(err => reject(err))
     )
 } 
-async function get(){
-    const test = await myaxios('https://covid-api.com/api/regions')
-    return test;
-}*/
 
+(async () => {
+    let regions = await myaxios('https://covid-api.com/api/regions');
+    regions = regions.data;
+
+    for(let country of regions) {
+        if(country.name == "France"){
+            let data = await myaxios(`https://covid-api.com/api/reports?date=${date}&iso=${country.iso}`);
+            let filtered = data.data.filter(el => el.region.province !== '')
+            let result =  filtered.map( el => { 
+                return { 
+                    province: el.region.province, 
+                    confirmed: el.confirmed, 
+                    deaths: el.deaths 
+                }
+            });
+            console.log(result);
+        }
+    }
+})()
+
+/*
 axios({
     method: 'get',
     url: 'https://covid-api.com/api/regions'
@@ -40,4 +57,4 @@ axios({
             })  
         }
     }
-})
+})*/
